25bfee96c9e167da294985bd848146288081e725
tatata812 <75353660+tatata812@users.noreply.github.com>
2024-04-12 16:40:16 +0900

    修正

---
+++ mdl-google.php
@@ -0,0 +1,10 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-W9Q430781Q"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-W9Q430781Q');
+  gtag('config', 'UA-18826752-12');
+</script>
